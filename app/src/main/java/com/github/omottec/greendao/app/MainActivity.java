107b8a9c1fa3fc6ac04ab04c940da5b55c2870e9
omottec <omottec@gmail.com>
2017-04-11 15:42:33 +0800

    rename package

--- app/src/main/java/com/github/omottec/greendao/app/MainActivity.java
+++ app/src/main/java/com/github/omottec/greendao/app/MainActivity.java
@@ -1,7 +1,9 @@
-package greendao.omottec.github.com.mygreendao;
+package com.github.omottec.greendao.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import greendao.omottec.github.com.mygreendao.R;
 
 public class MainActivity extends AppCompatActivity {
 
